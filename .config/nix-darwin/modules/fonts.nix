ba6884d55c810f147ad05c46114becf2a4fb413c
Malte Herrmann <malteherrmann.mail@gmail.com>
2025-04-09 21:24:44 +0200

    remove gh tracking and update fonts file

--- .config/nix-darwin/modules/fonts.nix
+++ .config/nix-darwin/modules/fonts.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 {
   fonts = {
-    # Enable font management
-    fontDir.enable = true;
-    
     # Install the specific fonts
-    fonts = with pkgs; [
+    packages = with pkgs; [
       (nerdfonts.override { fonts = [ "InconsolataGo" ]; })
     ];
   };
