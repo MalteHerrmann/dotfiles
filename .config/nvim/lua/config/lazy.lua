541ac86fcab9449624319a4a5abf568c5494e5b3
Malte Herrmann <malteherrmann.mail@gmail.com>
2025-10-12 00:22:26 +0200

    disable scrolling animations from snacks.nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,22 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- if the lazypath is not found, bootstrap it from source
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath
-  })
+	-- if the lazypath is not found, bootstrap it from source
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins" },
-  },
+	spec = {
+		-- add LazyVim and import its plugins
+		{
+			"LazyVim/LazyVim",
+			import = "lazyvim.plugins",
+		},
+		-- import/override with your plugins
+		{ import = "plugins" },
+	},
 })
