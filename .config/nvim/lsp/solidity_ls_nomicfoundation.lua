c40a15adec0adbdc8874b4cbeff5c0b9a522d6a1
Malte Herrmann-Talle <malte.herrmann@malte-herrmann-talle.fritz.box>
2026-05-07 18:43:13 +0200

    neovim: avoid Solidity LSP auto-formatting

--- .config/nvim/lsp/solidity_ls_nomicfoundation.lua
+++ .config/nvim/lsp/solidity_ls_nomicfoundation.lua
@@ -1,7 +1,25 @@
 ---@type vim.lsp.Config
+---
+---Taken from: https://github.com/neovim/nvim-lspconfig/blob/5a49a97/lsp/solidity_ls_nomicfoundation.lua
+---
 return {
 	cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
 	single_file_support = true,
 	filetypes = { "solidity" },
-	root_markers = { "foundry.toml", ".git" },
+	--- avoid auto-formatting by disabling them while attaching the LSP process
+	on_attach = function(client)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+	end,
+	root_markers = {
+		"hardhat.config.js",
+		"hardhat.config.ts",
+		"foundry.toml",
+		"remappings.txt",
+		"truffle.js",
+		"truffle-config.js",
+		"ape-config.yaml",
+		".git",
+		"package.json",
+	},
 }
